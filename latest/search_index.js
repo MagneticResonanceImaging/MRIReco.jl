a0ad609cfc0ff50af7af809fd90cec216f0d389d
autodocs <autodocs>
2018-10-12 20:10:17 +0000

    build based on 337db02

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "MRIReco",
+    "title": "MRIReco",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#MRIReco-1",
+    "page": "MRIReco",
+    "title": "MRIReco",
+    "category": "section",
+    "text": "Documentation goes here."
+},
+
+]}
