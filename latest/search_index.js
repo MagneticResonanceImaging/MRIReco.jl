63e6206d2ceb434df07733c5f018ebee9dee219d
autodocs <autodocs>
2018-10-14 16:30:14 +0000

    build based on 2b4c57e

--- latest/search_index.js
+++ latest/search_index.js
@@ -2,18 +2,50 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "index.html#",
-    "page": "MRIReco",
-    "title": "MRIReco",
+    "page": "Home",
+    "title": "Home",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "index.html#MRIReco-1",
-    "page": "MRIReco",
-    "title": "MRIReco",
+    "location": "index.html#MRIReco.jl-1",
+    "page": "Home",
+    "title": "MRIReco.jl",
     "category": "section",
-    "text": "Documentation goes here."
+    "text": "Magnetic Resonance Imaging Reconstruction"
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Home",
+    "title": "Introduction",
+    "category": "section",
+    "text": "MRIReco.jl is a Julia package for Magnetic Resonance Imaging (MRI). The Library provides a simulation and reconstruction framework where common and specific problems can be formulated and solved by direct or iterative optimization algorithms."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Start julia and open the package mode by entering ]. The enteradd https://github.com/tknopp/LinearSolver.jl\nadd https://github.com/MagneticResonanceImaging/MRIReco.jlThis will install the packages LinearSolver.jl, MRIReco.jl"
+},
+
+{
+    "location": "index.html#Usage-1",
+    "page": "Home",
+    "title": "Usage",
+    "category": "section",
+    "text": "See Getting Started for an introduction to using the package"
+},
+
+{
+    "location": "gettingStarted.html#",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "page",
+    "text": ""
 },
 
 ]}
