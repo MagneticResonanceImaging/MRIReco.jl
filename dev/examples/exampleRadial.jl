7b4c461fe0cba55b8605d0e7882c17d0f0366572
autodocs <autodocs>
2018-11-02 12:11:25 +0000

    build based on b4c2190

--- dev/examples/exampleRadial.jl
+++ dev/examples/exampleRadial.jl
@@ -18,7 +18,7 @@ aqData = simulation(I, params)
 
 # reco parameters
 params = Dict{Symbol, Any}()
-params[:reco] = "nfft"
+params[:reco] = "direct"
 params[:shape] = (N,N)
 Ireco = reconstruction(aqData, params)
 
