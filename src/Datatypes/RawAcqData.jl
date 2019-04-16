1041d3d0215672c49364f2ce12a2410938682b5b
Tobias Knopp <tobias@knoppweb.de>
2019-04-16 18:50:18 +0200

    simplify code

--- src/Datatypes/RawAcqData.jl
+++ src/Datatypes/RawAcqData.jl
@@ -1,68 +1,43 @@
 export RawAcquisitionData, EncodingCounters, AcquisitionHeader
 
-
-struct EncodingCounters
-  kspace_encode_step_1::Int16
-  kspace_encode_step_2::Int16
-  average::Int16
-  slice::Int16
-  contrast::Int16
-  phase::Int16
-  repetition::Int16
-  set::Int16
-  segment::Int16
-  user::NTuple{8,Int16}
+Base.@kwdef struct EncodingCounters
+  kspace_encode_step_1::Int16 = 0
+  kspace_encode_step_2::Int16 = 0
+  average::Int16 = 0
+  slice::Int16 = 0
+  contrast::Int16 = 0
+  phase::Int16 = 0
+  repetition::Int16 = 0
+  set::Int16 = 0
+  segment::Int16 = 0
+  user::NTuple{8,Int16} = ntuple(i->Int16(0),8)
 end
 
-function EncodingCounters(;kspace_encode_step_1=0, kspace_encode_step_2=0,
-            average=0, slice=0, contrast=0, phase=0, repetition=0, set=0,
-            segment=0, user=ntuple(i->Int16(0),8) )
-  return EncodingCounters(kspace_encode_step_1, kspace_encode_step_2,
-              average, slice, contrast, phase, repetition, set,
-              segment, user)
-end
-
-struct AcquisitionHeader
-  version::Int16
-  flags::Int64
-  measurement_uid::Int32
-  scan_counter::Int32
-  acquisition_time_stamp::Int32
-  physiology_time_stamp::NTuple{3,Int32}
-  number_of_samples::Int16
-  available_channels::Int16
-  active_channels::Int16
-  channel_mask::NTuple{16,Int64}
-  discard_pre::Int16
-  discard_post::Int16
-  center_sample::Int16
-  encoding_space_ref::Int16
-  trajectory_dimensions::Int16
-  sample_time_us::Float32
-  position::NTuple{3,Float32}
-  read_dir::NTuple{3,Float32}
-  phase_dir::NTuple{3,Float32}
-  slice_dir::NTuple{3,Float32}
-  patient_table_position::NTuple{3,Float32}
-  idx::EncodingCounters
-  user_int::NTuple{8,Int32}
-  user_float::NTuple{8,Float32}
-end
-
-function AcquisitionHeader(; version=0, flags=0, measurement_uid=0, scan_counter=0,
-   acquisition_time_stamp=0, physiology_time_stamp = ntuple(i->Int32(0),3),
-   number_of_samples=0, available_channels=0, active_channels=0, channel_mask=ntuple(i->Int64(0),16),
-   discard_pre=0, discard_post=0, center_sample=0, encoding_space_ref=0, trajectory_dimensions=0,
-   sample_time_us=0.0, position=ntuple(i->Float32(0),3), read_dir=ntuple(i->Float32(0),3),
-   phase_dir=ntuple(i->Float32(0),3), slice_dir=ntuple(i->Float32(0),3),
-   patient_table_position=ntuple(i->Float32(0),3), idx=EncodingCounters(),
-   user_int=ntuple(i->Int32(0),8), user_float=ntuple(i->Float32(0),8))
-  return AcquisitionHeader(version, flags, measurement_uid, scan_counter,
-     acquisition_time_stamp, physiology_time_stamp,
-     number_of_samples, available_channels, active_channels, channel_mask,
-     discard_pre, discard_post, center_sample, encoding_space_ref, trajectory_dimensions,
-     sample_time_us, position, read_dir, phase_dir, slice_dir, patient_table_position,
-     idx, user_int, user_float)
+Base.@kwdef struct AcquisitionHeader
+  version::Int16 = 0
+  flags::Int64 = 0
+  measurement_uid::Int32 = 0
+  scan_counter::Int32 = 0
+  acquisition_time_stamp::Int32 = 0
+  physiology_time_stamp::NTuple{3,Int32} = ntuple(i->Int32(0),3)
+  number_of_samples::Int16 = 0
+  available_channels::Int16 = 0
+  active_channels::Int16 = 0
+  channel_mask::NTuple{16,Int64} = ntuple(i->Int64(0),16)
+  discard_pre::Int16 = 0
+  discard_post::Int16 = 0
+  center_sample::Int16 = 0
+  encoding_space_ref::Int16 = 0
+  trajectory_dimensions::Int16 = 0
+  sample_time_us::Float32 = 0.0
+  position::NTuple{3,Float32} = ntuple(i->Float32(0),3)
+  read_dir::NTuple{3,Float32} = ntuple(i->Float32(0),3)
+  phase_dir::NTuple{3,Float32} = ntuple(i->Float32(0),3)
+  slice_dir::NTuple{3,Float32} = ntuple(i->Float32(0),3)
+  patient_table_position::NTuple{3,Float32} = ntuple(i->Float32(0),3)
+  idx::EncodingCounters = EncodingCounters()
+  user_int::NTuple{8,Int32} = ntuple(i->Int32(0),8)
+  user_float::NTuple{8,Float32} = ntuple(i->Float32(0),8)
 end
 
 mutable struct Profile
