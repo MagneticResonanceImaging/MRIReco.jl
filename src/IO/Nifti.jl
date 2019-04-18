8cca63ac2888c9080dfe67819c5212be68dff9be
Tobias Knopp <tobias@knoppweb.de>
2019-04-18 02:38:01 +0200

    add orientation to Nifti

--- src/IO/Nifti.jl
+++ src/IO/Nifti.jl
@@ -9,7 +9,11 @@ end
 function saveImage(filename::String, im::AbstractArray{T,5}, makeAbs::Bool=false) where T
   voxel_size = ustrip.(uconvert.(u"m", pixelspacing(im)[1:3]))
 
-  ni = NIVolume(makeAbs ? abs.(im) : im; voxel_size=voxel_size)
-  # TODO add orientation   orientation=orientation, dim_info=dim_info)
+  orientation = zeros(Float32, 3, 4)
+  orientation[1,1] = orientation[2,2] = orientation[3,3] = 1.0
+
+  ni = NIVolume(makeAbs ? abs.(im) : im; voxel_size=voxel_size,
+                              orientation = orientation)
+  # TODO add  dim_info=dim_info)
   niwrite(filename, ni)
 end
