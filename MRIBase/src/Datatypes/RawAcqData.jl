2eae075aae2b75d8b0e99d2baf4196c511cd5067
aTrotier <a.trotier@gmail.com>
2023-01-05 11:57:00 +0100

    acqData.Fov in mm + revert div 1000.0

--- MRIBase/src/Datatypes/RawAcqData.jl
+++ MRIBase/src/Datatypes/RawAcqData.jl
@@ -294,7 +294,7 @@ function AcquisitionData(f::RawAcquisitionData; estimateProfileCenter::Bool=fals
   acq = AcquisitionData(tr, kdata,
                           idx = subsampleIdx,
                           encodingSize = ntuple(d->f.params["encodedSize"][d], ndims(tr[1])),
-                          fov = Float64.(ntuple(d->f.params["encodedFOV"][d]/1000.0, 3)))
+                          fov = Float64.(ntuple(d->f.params["encodedFOV"][d], 3)))
 
   if OffsetBruker
     ROT = [[f.profiles[1].head.read_dir...] [f.profiles[1].head.phase_dir...] [f.profiles[1].head.slice_dir...]]
@@ -313,7 +313,7 @@ converts `acqData` into the equivalent `RawAcquisitionData` object.
 """
 function RawAcquisitionData(acqData::AcquisitionData{T,D}) where {T,D}
   # XML header
-  params = minimalHeader(ntuple(d->acqData.encodingSize[d],D), T.(acqData.fov) .* 1000.0, tr_name=string(trajectory(acqData,1)))
+  params = minimalHeader(ntuple(d->acqData.encodingSize[d],D), T.(acqData.fov), tr_name=string(trajectory(acqData,1)))
   # acquisition counter
   counter = 1
   # profiles
