f44409facbea3a7cfeac73729f5d9c68e5d60c08
Jakob Asslaender <jakob.asslaender@nyumc.org>
2023-03-21 18:46:16 -0400

    Change input type from Vector etc. to AbstractVector etc.

--- MRIOperators/src/SensitivityOp.jl
+++ MRIOperators/src/SensitivityOp.jl
@@ -1,40 +1,41 @@
-function prod_smap!(y::Vector{T}, smaps::Matrix{T}, x::Vector{T}, numVox::Int64, numChan::Int64, numContr::Int=1) where T
-    x = reshape(x,numVox,numContr)
-    y_ = reshape(y,numVox,numContr,numChan)
-    @inbounds for j=1:numChan
-        @inbounds for i=1:numContr
-          @inbounds for k=1:numVox
-            y_[k,i,j] = x[k,i] * smaps[k,j]
-          end
-        end
-    end
-    return y
+export SensitivityOp
+
+function prod_smap!(y::AbstractVector{T}, smaps::AbstractMatrix{T}, x::AbstractVector{T}, numVox, numChan, numContr=1) where T
+  x_ = reshape(x,numVox,numContr)
+  y_ = reshape(y,numVox,numContr,numChan)
+
+  @assert size(smaps) == size(y_)[1,3]
+
+  @inbounds for i ∈ CartesianIndices(y_)
+    y_[i] = x_[i[1],i[2]] * smaps[i[1],i[3]]
+  end
+  return y
 end
 
-function ctprod_smap!(y::Vector{T}, smapsC::Matrix{T}, x::Vector{T}, numVox::Int64, numChan::Int64, numContr::Int=1) where T
-    x = reshape(x,numVox,numContr,numChan)
-    y_ = reshape(y,numVox,numContr)
-    y_ .= 0
-    @inbounds for j=1:numChan
-      @inbounds for i=1:numContr
-        @inbounds for k=1:numVox
-          y_[k,i] += x[k,i,j] * smapsC[k,j]
-        end
-      end
+function ctprod_smap!(y::AbstractVector{T}, smapsC::AbstractMatrix{T}, x::AbstractVector{T}, numVox, numChan, numContr=1) where T
+  x_ = reshape(x,numVox,numContr,numChan)
+  y_ = reshape(y,numVox,numContr)
+
+  @assert size(smapsC) == size(x_)[1,3]
+
+  y_ .= 0
+  @inbounds for i ∈ CartesianIndices(x_)
+    y_[i[1],i[2]] += x_[i] * smapsC[i[1],i[3]]
   end
   return y
 end
 
 
 """
-    SensitivityOp(sensMaps::Matrix{ComplexF64}, numEchoes::Int=1)
+  SensitivityOp(sensMaps::AbstractMatrix{T}, numContr=1)
+
 builds a `LinearOperator` which performs multiplication of a given image with
 the coil sensitivities specified in `sensMaps`
 # Arguments
-* `sensMaps::Matrix{ComplexF64}`  - sensitivity maps ( 1. dim -> voxels, 2. dim-> coils)
-* `numEchoes`                     - number of contrasts to which the operator will be applied
+* `sensMaps`    - sensitivity maps ( 1. dim -> voxels, 2. dim-> coils)
+* `numEchoes`   - number of contrasts to which the operator will be applied
 """
-function SensitivityOp(sensMaps::Matrix{T}, numContr::Int=1) where T
+function SensitivityOp(sensMaps::AbstractMatrix{T}, numContr=1) where T
     numVox, numChan = size(sensMaps)
     sensMapsC = conj.(sensMaps)
     return LinearOperator{T}(numVox*numContr*numChan, numVox*numContr, false, false,
@@ -44,14 +45,15 @@ function SensitivityOp(sensMaps::Matrix{T}, numContr::Int=1) where T
 end
 
 """
-    SensitivityOp(sensMaps::Array{T,4}, numContr::Int=1) where T
+  SensitivityOp(sensMaps::AbstractArray{T,4}, numContr=1)
+
 builds a `LinearOperator` which performs multiplication of a given image with
 the coil sensitivities specified in `sensMaps`
 # Arguments
-* `sensMaps::Array{T,4}`  - sensitivity maps ( 1.-3. dim -> voxels, 4. dim-> coils)
-* `numContr`             - number of contrasts to which the operator will be applied
+* `sensMaps`  - sensitivity maps ( 1.-3. dim -> voxels, 4. dim-> coils)
+* `numContr`  - number of contrasts to which the operator will be applied
 """
-function SensitivityOp(sensMaps::Array{T,4}, numContr::Int=1) where T #{T,D}
+function SensitivityOp(sensMaps::AbstractArray{T,4}, numContr=1) where T #{T,D}
   sensMaps_mat = reshape(sensMaps, div(length(sensMaps),size(sensMaps,4)),size(sensMaps,4))
   return SensitivityOp(sensMaps_mat, numContr)
 end
