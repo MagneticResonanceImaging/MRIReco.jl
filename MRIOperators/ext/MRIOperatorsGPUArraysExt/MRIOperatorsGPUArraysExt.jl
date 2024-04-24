de51024fcaa7a1116d33be68fd2ff9f15eb27e6f
nHackel <nHackel@users.noreply.github.com>
2024-04-24 14:06:51 +0200

    Fix GPU FFT params

--- MRIOperators/ext/MRIOperatorsGPUArraysExt/MRIOperatorsGPUArraysExt.jl
+++ MRIOperators/ext/MRIOperatorsGPUArraysExt/MRIOperatorsGPUArraysExt.jl
@@ -6,7 +6,7 @@ include("ExplicitOp.jl")
 include("Shutter.jl")
 include("SensitivityOp.jl")
 
-MRIOperators.fftParams(::Type{<:AbstractGPUArray}) = (;:flags => FFTW.MEASURE)
+MRIOperators.fftParams(::Type{<:AbstractGPUArray}) = (;)
 
 
 end # module
