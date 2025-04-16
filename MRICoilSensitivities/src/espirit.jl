c12696b2f86c2787e1c34fb5ec4d6e32f8b9e64b
Jakob Asslaender <jakob.asslaender@nyumc.org>
2025-04-16 16:39:45 -0400

    Auto-format (no changes to the code)

--- MRICoilSensitivities/src/espirit.jl
+++ MRICoilSensitivities/src/espirit.jl
@@ -30,10 +30,10 @@ Where SENSE meets GRAPPA"](https://doi.org/10.1002/mrm.24751)). The matlab code 
   * `echo::Int = 1` - select the k-space corresponding to the echo
   * `rep::Int = 1` - select the k-space corresponding to the rep
 """
-function espirit(acqData::AcquisitionData{T,D}, ksize::NTuple{D,Int} = ntuple(d->6,D), ncalib::Int = 24,
-                 imsize::NTuple{D,Int} = encodingSize(acqData);
-                 nmaps::Int = 1,
-                 echo::Int = 1, rep::Int = 1, kargs...) where {T,D}
+function espirit(acqData::AcquisitionData{T,D}, ksize::NTuple{D,Int}=ntuple(d -> 6, D), ncalib::Int=24,
+  imsize::NTuple{D,Int}=encodingSize(acqData);
+  nmaps::Int=1,
+  echo::Int=1, rep::Int=1, kargs...) where {T,D}
 
   if !isCartesian(trajectory(acqData, 1))
     @error "espirit does not yet support non-cartesian sampling"
@@ -56,11 +56,11 @@ function espirit(acqData::AcquisitionData{T,D}, ksize::NTuple{D,Int} = ntuple(d-
     # form zeropadded array with kspace data
     kdata = zeros(Complex{T}, prod(imsize), numChan)
     for coil = 1:numChan
-      kdata[idx, coil] .= kData(acqData, echo, coil, slice;rep=rep)
+      kdata[idx, coil] .= kData(acqData, echo, coil, slice; rep=rep)
     end
     kdata = reshape(kdata, imsize..., numChan)
 
-    calib = crop(kdata, ntuple(d->ncalib,D))
+    calib = crop(kdata, ntuple(d -> ncalib, D))
 
     maps[CartesianIndices(imsize), slice, :, :] .= espirit(calib, imsize, ksize; kargs...)
   end
@@ -70,13 +70,13 @@ function espirit(acqData::AcquisitionData{T,D}, ksize::NTuple{D,Int} = ntuple(d-
   end
 
   if nmaps == 1
-    maps = maps[:,:,:,:,1]
+    maps = maps[:, :, :, :, 1]
   end
   return maps
 end
 
-function espirit(calibData::Array{T}, imsize::NTuple{D,Int}, ksize::NTuple{D,Int} = ntuple(_ -> 6, D)
-  ; eigThresh_1::Number = 0.02, eigThresh_2::Number = 0.95, nmaps = 1, use_poweriterations = true) where {T,D}
+function espirit(calibData::Array{T}, imsize::NTuple{D,Int}, ksize::NTuple{D,Int}=ntuple(_ -> 6, D)
+  ; eigThresh_1::Number=0.02, eigThresh_2::Number=0.95, nmaps=1, use_poweriterations=true) where {T,D}
   nc = size(calibData)[end]
 
   # compute calibration matrix, perform SVD and convert right singular vectors
@@ -86,11 +86,11 @@ function espirit(calibData::Array{T}, imsize::NTuple{D,Int}, ksize::NTuple{D,Int
 
   # crop kernels and compute eigenvalue decomposition in images space
   # to get sensitivity maps
-  maps, W = kernelEig(k[CartesianIndices((ksize..., nc)), 1:idx], imsize, nmaps; use_poweriterations = use_poweriterations)
+  maps, W = kernelEig(k[CartesianIndices((ksize..., nc)), 1:idx], imsize, nmaps; use_poweriterations=use_poweriterations)
 
   # sensitivity maps correspond to eigen vectors with a singular value of 1
-  msk = zeros(Bool,size(W))
-  msk[W .> eigThresh_2] .= true
+  msk = zeros(Bool, size(W))
+  msk[W.>eigThresh_2] .= true
   maps .*= msk
   maps_ = fftshift(maps, 1:length(imsize))
 
@@ -103,12 +103,12 @@ end
 #   into k-space kernels
 #
 function dat2Kernel(data::Array{T,M}, ksize::NTuple{D,Int64}) where {T,D,M}
-  M != D+1 && error("data must have 1 dimension more than the length of ksize")
+  M != D + 1 && error("data must have 1 dimension more than the length of ksize")
 
   nc = size(data)[end]
 
   A = im2row(data, ksize)
-  A = reshape(A, size(A,1), :)
+  A = reshape(A, size(A, 1), :)
 
   nblas = BLAS.get_num_threads()
   usv = try
@@ -139,7 +139,7 @@ function kernelEig(kernel::Array{T}, imsize::Tuple, nmaps=1; use_poweriterations
   sizePadded = 2 .* ksize
   nc = kern_size[end-1]
   nv = kern_size[end]
-  flip_nc_nv  = [1:length(ksize); length(ksize) + 2; length(ksize) + 1]
+  flip_nc_nv = [1:length(ksize); length(ksize) + 2; length(ksize) + 1]
 
   nmaps = min(nc, nv, nmaps)
 
@@ -147,7 +147,7 @@ function kernelEig(kernel::Array{T}, imsize::Tuple, nmaps=1; use_poweriterations
   kernel = permutedims(kernel, flip_nc_nv)
   kernel = reshape(kernel, :, nc)
 
-  usv = svd(kernel, full = (size(kernel, 1) < size(kernel, 2)))
+  usv = svd(kernel, full=(size(kernel, 1) < size(kernel, 2)))
 
   kernel = kernel * usv.V
   kernel = reshape(kernel, ksize..., nv, nc)
@@ -158,13 +158,13 @@ function kernelEig(kernel::Array{T}, imsize::Tuple, nmaps=1; use_poweriterations
 
   fftplan = plan_fft(kernel_i, 1:ndims(kernel_i)-1; flags=FFTW.MEASURE, num_threads=Threads.nthreads())
 
-  for iv ∈ axes(kernel, length(kern_size)-1)
-    @views kernel_k[CartesianIndices(ksize),:] .= kernel[CartesianIndices(ksize),iv,:]
+  for iv ∈ axes(kernel, length(kern_size) - 1)
+    @views kernel_k[CartesianIndices(ksize), :] .= kernel[CartesianIndices(ksize), iv, :]
     mul!(kernel_i, fftplan, kernel_k)
 
-    @floop for j ∈ axes(kernel_i,ndims(kernel_i)), ix ∈ CartesianIndices(sizePadded)
-      @inbounds @simd for i ∈ axes(kernel_i,ndims(kernel_i))
-        kern2_[i,j,ix] += conj(kernel_i[ix,i]) * kernel_i[ix,j]
+    @floop for j ∈ axes(kernel_i, ndims(kernel_i)), ix ∈ CartesianIndices(sizePadded)
+      @inbounds @simd for i ∈ axes(kernel_i, ndims(kernel_i))
+        kern2_[i, j, ix] += conj(kernel_i[ix, i]) * kernel_i[ix, j]
       end
     end
   end
@@ -180,24 +180,24 @@ function kernelEig(kernel::Array{T}, imsize::Tuple, nmaps=1; use_poweriterations
 
   idx_center = collect(CartesianIndices(sizePadded) .- CartesianIndex(sizePadded .÷ 2) .+ CartesianIndex(imsize .÷ 2))
   [idx_center[i] = CartesianIndex(mod1.(Tuple(idx_center[i]) .+ cld.(imsize, 2), imsize)) for i in eachindex(idx_center)]
-  @views ifftshift!(kern2[:,:,idx_center], kern2_, 3:ndims(kern2_))
-  
+  @views ifftshift!(kern2[:, :, idx_center], kern2_, 3:ndims(kern2_))
+
   reshape(ifft!(reshape(kern2, nc^2, imsize...), 2:ndims(kern2)-1), nc, nc, imsize...)
   kern2 .*= prod(imsize) / prod(sizePadded) / prod(ksize)
 
 
-  eigenVecs = Array{     T }(undef, imsize..., nc, nmaps)
-  eigenVals = Array{real(T)}(undef, imsize...,  1, nmaps)
+  eigenVecs = Array{T}(undef, imsize..., nc, nmaps)
+  eigenVals = Array{real(T)}(undef, imsize..., 1, nmaps)
 
   nblas = BLAS.get_num_threads()
   try
     BLAS.set_num_threads(1)
-    b    = [randn(T, nc)  for _=1:Threads.nthreads()]
-    bᵒˡᵈ = [similar(b[1]) for _=1:Threads.nthreads()]
+    b = [randn(T, nc) for _ = 1:Threads.nthreads()]
+    bᵒˡᵈ = [similar(b[1]) for _ = 1:Threads.nthreads()]
 
     @floop for n ∈ CartesianIndices(imsize)
-      if use_poweriterations && nmaps==1
-        S, U = power_iterations!(view(kern2,:, :, n), b=b[Threads.threadid()], bᵒˡᵈ=bᵒˡᵈ[Threads.threadid()])
+      if use_poweriterations && nmaps == 1
+        S, U = power_iterations!(view(kern2, :, :, n), b=b[Threads.threadid()], bᵒˡᵈ=bᵒˡᵈ[Threads.threadid()])
         # The following uses the method from IterativeSolvers.jl but is currently slower and allocating more
         # this is probably because we pre-allocate bᵒˡᵈ
         #S, U = RegularizedLeastSquares.IterativeSolvers.powm!(view(kern2, :, :, n), b[Threads.threadid()], maxiter=5)
@@ -205,8 +205,8 @@ function kernelEig(kernel::Array{T}, imsize::Tuple, nmaps=1; use_poweriterations
         U .*= transpose(exp.(-1im .* angle.(U[1])))
         @views eigenVals[n, 1, :] .= real.(S)
       else
-        S, U = eigen!(view(kern2, :, :, n); sortby = (λ) -> -abs(λ))
-        U = @view U[:,1:nmaps]
+        S, U = eigen!(view(kern2, :, :, n); sortby=(λ) -> -abs(λ))
+        U = @view U[:, 1:nmaps]
         U .*= transpose(exp.(-1im .* angle.(@view U[1, :])))
         @views eigenVals[n, 1, :] .= real.(S[1:nmaps])
       end
@@ -242,8 +242,8 @@ Power iterations to determine the maximum eigenvalue of a normal operator or squ
 maximum eigenvalue of the operator
 """
 function power_iterations!(A;
-  b = randn(eltype(A), size(A,2)),
-  bᵒˡᵈ = similar(b),
+  b=randn(eltype(A), size(A, 2)),
+  bᵒˡᵈ=similar(b),
   rtol=1e-6, maxiter=1000, verbose=false)
 
   b ./= norm(b)
@@ -258,7 +258,7 @@ function power_iterations!(A;
     b ./= norm(b)
 
     #verbose && println("iter = $i; λ = $λ")
-    abs(λ/λᵒˡᵈ - 1) < rtol && break
+    abs(λ / λᵒˡᵈ - 1) < rtol && break
   end
 
   return λ, b
@@ -280,7 +280,7 @@ function im2row(img::Array{T,M}, winSize::NTuple{N,Int}) where {N,M,T}
   cnt = 0
   for ms ∈ CartesianIndices(winSize)
     cnt += 1
-    res[:, cnt, :] .= reshape(img[m .+ ms, :], :, ncoils)
+    res[:, cnt, :] .= reshape(img[m.+ms, :], :, ncoils)
   end
   return res
 end
@@ -301,7 +301,7 @@ end
   findIndices()
   Calculates the indices which place the center of k-space sampled on oldEnc in the correct place w.r.t a grid of size newEnc
 """
-function findIndices(newEnc::NTuple{D,Int64}, oldEnc::NTuple{D,Int64}) where D
+function findIndices(newEnc::NTuple{D,Int64}, oldEnc::NTuple{D,Int64}) where {D}
   shift = (newEnc .- oldEnc) .÷ 2
 
   oldCart = CartesianIndices(oldEnc) .+ CartesianIndex(shift)
