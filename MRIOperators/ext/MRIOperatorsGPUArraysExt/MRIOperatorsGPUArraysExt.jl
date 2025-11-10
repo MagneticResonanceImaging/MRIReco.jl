42ce909b88fdfd80738dfde381335753bd257825
Niklas Hackelberg <nHackel@users.noreply.github.com>
2025-11-10 12:19:42 +0100

    Specialise fft parameter for different AbstractNFFT backend

--- MRIOperators/ext/MRIOperatorsGPUArraysExt/MRIOperatorsGPUArraysExt.jl
+++ MRIOperators/ext/MRIOperatorsGPUArraysExt/MRIOperatorsGPUArraysExt.jl
@@ -2,13 +2,13 @@ module MRIOperatorsGPUArraysExt
 
 using MRIOperators, GPUArrays, KernelAbstractions
 using MRIOperators.FFTW, MRIOperators.LinearAlgebra
+using MRIOperators.NFFT
 
 include("ExplicitOp.jl")
 include("Shutter.jl")
 include("SensitivityOp.jl")
 include("FieldmapNFFTOp.jl")
 
-MRIOperators.fftParams(::Type{<:AbstractGPUArray}) = (;)
-
+fftParams(::NFFT.NFFTBackend, ::Type{<:AbstractGPUArray}) = (;)
 
 end # module
--- MRIOperators/src/MRIOperators.jl
+++ MRIOperators/src/MRIOperators.jl
@@ -83,7 +83,9 @@ function Base.copy(S::LinearOperator{T}) where T
   deepcopy(S)
 end
 
-fftParams(::Type{<:AbstractArray}) = (;:flags => FFTW.MEASURE)
+fftParams(T::Type{<:AbstractArray}) = fftParams(NFFT.active_backend(), T)
+fftParams(::AbstractNFFTBackend, ::Type{<:AbstractArray}) = (;:flags => FFTW.MEASURE)
+
 
 # https://github.com/JuliaLang/julia/issues/35543
 stripParameters(arrT::Type{<:AbstractArray}) = Base.typename(arrT).wrapper
